63dffe5a5e1261e6db6218319d1305d93a36b41b
Eipek95 <eipek1995@gmail.com>
2022-09-12 15:14:26 +0300

    List CarDetail And Refactor

--- ReCapFrontEnd/src/app/services/car.service.ts
+++ ReCapFrontEnd/src/app/services/car.service.ts
@@ -3,16 +3,30 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Car } from '../models/car';
+import { Carimage } from '../models/carimage';
+import { SingleResponseModel } from '../models/singleResponseModel';
+import { CarDetailDto } from '../models/cardetaildto';
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  private apiUrl='https://localhost:7199/api/Cars/getcardetails';
+  private apiUrl="https://localhost:7199/api/";
   constructor(private httpClient:HttpClient) { }
 
+  getCars(): Observable<ListResponseModel<CarDetailDto>> {
+   let newPath=this.apiUrl+"Cars/getcar";
+    return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
+  }
 
-  getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+
+  getCarImagesByCarId(id: number): Observable<ListResponseModel<Carimage>> {
+    let newPath=this.apiUrl+"CarImages/getcardetailsbyid?id="+id
+    return this.httpClient.get<ListResponseModel<Carimage>>(newPath);
+  }
+
+  getCarDetailsByCarId(id: number): Observable<SingleResponseModel<CarDetailDto>> {
+    let carDetailPath = this.apiUrl + "Cars/getcardetailsbyid?id=" + id;
+    return this.httpClient.get<SingleResponseModel<CarDetailDto>>(carDetailPath);
   }
 }
