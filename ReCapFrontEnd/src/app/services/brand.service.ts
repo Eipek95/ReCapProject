320ee14c373a947c8770fcafc108b9e27cd66b6c
Eipek95 <eipek1995@gmail.com>
2022-09-15 22:21:37 +0300

    Add Brand Add Form and Manager and toastrservice

--- ReCapFrontEnd/src/app/services/brand.service.ts
+++ ReCapFrontEnd/src/app/services/brand.service.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Brand } from '../models/brand';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BrandService {
+  private apiUrl = 'https://localhost:7199/api/';
+  constructor(private httpClient: HttpClient) {}
 
-  private apiUrl='https://localhost:7199/api/Brands/getbrand';
-  constructor(private httpClient:HttpClient) { }
-
-  getBrands():Observable<ListResponseModel<Brand>>{
-    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+  getBrands(): Observable<ListResponseModel<Brand>> {
+    let newPath = this.apiUrl + 'Brands/getbrand';
+    return this.httpClient.get<ListResponseModel<Brand>>(newPath);
+  }
+  add(brand: Brand): Observable<ResponseModel> {
+    let newPath = this.apiUrl + 'Brands/addBrand';
+    return this.httpClient.post<ResponseModel>(newPath, brand);
   }
 }
