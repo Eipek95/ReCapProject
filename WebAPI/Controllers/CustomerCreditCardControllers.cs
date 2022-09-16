65180c2b28232625a90a68a8ba2017478b043c60
Eipek95 <eipek1995@gmail.com>
2022-09-16 22:09:14 +0300

    CustomerCreditCard And Payment Controllers

---
+++ WebAPI/Controllers/CustomerCreditCardControllers.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerCreditCardControllers : ControllerBase
+    {
+        ICustomerCreditCardService _customerCreditCardService;
+
+        public CustomerCreditCardControllers(ICustomerCreditCardService customerCreditCardService)
+        {
+            _customerCreditCardService = customerCreditCardService;
+        }
+        [HttpGet("getsavedcreditcardsbycustomerid")]
+        public IActionResult GetSavedCreditCardsByCustomerId(int customerId)
+        {
+            var result = _customerCreditCardService.GetSavedCreditCardsByCustomerId(customerId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Message);//kullanıcı bilgilendirmesi yapar.swagger dökümantasyonua bak.400 de data
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/PaymentControllers.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentControllers : ControllerBase
+    {
+        IPaymentService _paymentService;
+
+        public PaymentControllers(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+        [HttpGet("pay")]
+        public IActionResult Pay(CreditCard payment,int customerId,int amount)
+        {
+            var result = _paymentService.Pay(payment,customerId,amount);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+    }
+}
